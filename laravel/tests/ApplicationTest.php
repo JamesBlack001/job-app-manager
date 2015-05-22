9f419c70b3f8fc151217cfd8fad9f31546485e42
James Black <james_black001@hotmail.co.uk>
2015-05-22 14:50:32 +0100

    removed some files

--- laravel/tests/ApplicationTest.php
+++ laravel/tests/ApplicationTest.php
@@ -13,5 +13,31 @@ class ApplicationTest extends TestCase {
 
 		$this->assertEquals(200, $response->getStatusCode());
 	}
+	public function testResponseOk()
+	{
+		$response = $this->call('GET', route('jobs_path'));
+
+		$this->assertResponseOk();//checks status code is 200
+	}
+	public function testRoutesStatus()
+	{
+		$response = $this->call('GET', route('jobs_path'));
+
+		$this->assertResponseOk();//checks status code is 200
+
+		$response = $this->call('GET', route('job_path'));
+
+		$this->assertResponseOk();//checks status code is 200
+
+		$response = $this->call('GET', route('job_edit'));
+
+		$this->assertResponseOk();//checks status code is 200
+	}
+	public function testRouteJobUpdate()
+	{
+		$response = $this->call('PUT', route('job_update'));
+
+		$this->assertEquals(500, $response->getStatusCode());//checks status code is 200
+	}
 
 }
